26af184264c9c23351c25c49f752f6666824b496
NINJA-Ai <68237917+NINJA-Ai@users.noreply.github.com>
2021-04-25 19:35:01 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+function preload()
+{
+
+}
+function setup()
+{
+    createCanvas(800, 800)
+    
+}
+function draw()
+{
+    circle(30, 770, 50);
+    circle(770, 30, 50);
+    circle(30, 30, 50);
+    circle(770, 770, 50);
+
+}
