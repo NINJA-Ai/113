a9620ed8df3eba5dc5c199c437396559ef2658b9
NINJA-Ai <68237917+NINJA-Ai@users.noreply.github.com>
2021-04-30 20:14:53 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -4,11 +4,16 @@ function preload()
 }
 function setup()
 {
-    createCanvas(800, 800)
-    
+    canvas = createCanvas(300, 300);
+    canvas.center(); 
+    video = createCapture(VIDEO); 
+    video.size(300, 300);
+    video.hide();
+
 }
 function draw()
 {
+    
     circle(30, 770, 50);
     circle(770, 30, 50);
     circle(30, 30, 50);
