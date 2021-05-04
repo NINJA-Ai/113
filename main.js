d4838e9b0c8f1604b09862a004131053da9995b3
NINJA-Ai <68237917+NINJA-Ai@users.noreply.github.com>
2021-05-04 12:56:31 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -12,23 +12,23 @@ function setup()
 
 
 function draw() {
-  image(video, 230, 150, 220, 200);
+  image(video, 110, 110, 420, 250);
 
-  fill(255, 0, 0);
+  fill(0, 100, 0);
   stroke(255, 0, 0);
 
-  circle(50, 50, 80);
-  circle(50, 430, 80);
-  circle(590, 50, 80);
-  circle(590, 430, 80);
+  circle(80, 80, 80);
+  circle(80, 400, 80);
+  circle(560, 80, 80);
+  circle(560, 400, 80);
   
-  fill(0, 128, 0);
+  fill(0, 102, 153);
   stroke(0, 128, 0);
  
-  rect(90, 40, 460, 20);
-  rect(90, 420, 460, 20);
-  rect(40, 90, 20, 300);
-  rect(580, 90, 20, 300);
+  rect(0, 0, 640, 20);
+  rect(0, 0, 20, 480);
+  rect(620, 0, 640, 480);
+  rect(0, 460, 640, 480);
 }
 function take_snapshot(){    
     save('your_pic.png');
